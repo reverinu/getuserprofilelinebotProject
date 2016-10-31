28d89832b650ee94e8dfaa365f247b6a79a228f5
reverinu <reverinudog@yahoo.co.jp>
2016-11-01 01:29:27 +0900

    bz

--- web/callback.php
+++ web/callback.php
@@ -43,9 +43,17 @@ if ("message" == $event->type) {            //一般的なメッセージ(文字
     	return;
     	
     
+    } else if ("@profile" == $event->message->text) {
+    	$response = $bot->getProfile($event->source->userId);
+    	if ($response->isSucceeded()) {
+    		$profile = $response->getJSONDecodedBody();
+    		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($profile['displayName']);
+    		$response2 = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+		}
+    	
     } else if ("text" == $event->message->type) {
 
-        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
+        //$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
 
     } else {
 
