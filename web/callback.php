7ba6079e4c7b917ba0ff8e0e8c88339340fd7c2c
reverinu <reverinudog@yahoo.co.jp>
2016-11-01 11:37:14 +0900

    ｆｓｆｓｆｓ

--- web/callback.php
+++ web/callback.php
@@ -32,7 +32,7 @@ if ("message" == $event->type) {            //一般的なメッセージ(文字
     	if("group" == $event->source->type) {
     		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->source->groupId);
     		$response2 = $bot->replyMessage($event->replyToken, $textMessageBuilder);
-    		$response3 = $bot->pushMessage($event->source->groupId, $textMessageBuilder);
+    		$response3 = $bot->pushMessage('R9b7dbfd03cbc9c2e4ab3624051c6b011', $textMessageBuilder);
     		$response = $bot->leaveGroup($event->source->groupId);
     	} else if("room" == $event->source->type) {
     		//$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->source->roomId);
