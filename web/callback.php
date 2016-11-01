09630d0e0442b543c0f67be7bb1c2db74b5fdab6
reverinu <reverinudog@yahoo.co.jp>
2016-11-02 01:08:32 +0900

    ribo

--- web/callback.php
+++ web/callback.php
@@ -20,42 +20,60 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '3095c84a53d38913b6716
 
 //イベントタイプ判別
 
-if ("@join" == $event->message->text) {
+if ("message" == $event->type) {            //一般的なメッセージ(文字・イメージ・音声・位置情報・スタンプ含む)
+
+    if ("@join" == $event->message->text) {
     	$response = $bot->getProfile($event->source->userId);
     	if ($response->isSucceeded()) {
     		$profile = $response->getJSONDecodedBody();
     		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($profile['displayName'] . "はゲームに参加したよ！");
     		$response2 = $bot->replyMessage($event->replyToken, $textMessageBuilder);
-		}
+		  }
 
-} elseif ("text" == $event->message->type) {// テキストを受け取ったら
-  if("group" == $event->source->type) {
-    //groupの話
-    $action = new MessageTemplateActionBuilder("NU", "nu")
-    $action1 = new MessageTemplateActionBuilder("NO", "no")
-    $action2 = new MessageTemplateActionBuilder("NE", "ne")
+    } else if ("text" == $event->message->type) {
 
-    $button = new ButtonTemplateBuilder("ひげ", "ひげげ", "https://" . $_SERVER['SERVER_NAME'] . "/kyojin.jpeg", [$action, $action1, $action2]);
-    $button_message = new TemplateMessageBuilder("ひげがここにボタンで表示されてるよ", $button);
+      if("group" == $event->source->type) {
+        //groupの話
+        $action = new MessageTemplateActionBuilder("NU", "nu")
+        $action1 = new MessageTemplateActionBuilder("NO", "no")
+        $action2 = new MessageTemplateActionBuilder("NE", "ne")
+
+        $button = new ButtonTemplateBuilder("ひげ", "ひげげ", "https://" . $_SERVER['SERVER_NAME'] . "/kyojin.jpeg", [$action, $action1, $action2]);
+        $button_message = new TemplateMessageBuilder("ひげがここにボタンで表示されてるよ", $button);
 
 
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("はなげ");
-    $response = $bot->pushMessage('R9b7dbfd03cbc9c2e4ab3624051c6b011', $textMessageBuilder);
-  } else {
-      $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ごめん、わかんなーい(*´ω｀*)");
-  }
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("はなげ");
+        $response = $bot->pushMessage('R9b7dbfd03cbc9c2e4ab3624051c6b011', $textMessageBuilder);
+      } else if("room" == $event->source->type) {
+
+    	}
+
+    } else {
+
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ごめん、わかんなーい(*´ω｀*)");
+    }
 
 } elseif ("follow" == $event->type) {        //お友達追加時
+
     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("よろしくー");
+
 } elseif ("join" == $event->type) {           //グループに入ったときのイベント
+
     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('こんにちは よろしくー');
+
 } elseif ('beacon' == $event->type) {         //Beaconイベント
+
     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Godanがいんしたお(・∀・) ');
+
 } else {
-    //なにもしない
 }
+
+
+
 //$response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
 
 syslog(LOG_EMERG, print_r($event->replyToken, true));
+
 syslog(LOG_EMERG, print_r($response, true));
+
 return;
