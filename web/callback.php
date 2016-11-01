c6f6e785b5dc22f2fdc038ea3778cf5974e1102a
reverinu <reverinudog@yahoo.co.jp>
2016-11-01 11:42:24 +0900

    匿名チャットやってみる

--- web/callback.php
+++ web/callback.php
@@ -30,13 +30,14 @@ if ("message" == $event->type) {            //一般的なメッセージ(文字
 
     if ("@bye" == $event->message->text && ("group" == $event->source->type || "room" == $event->source->type)) {
     	if("group" == $event->source->type) {
-    		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->source->groupId);
-    		$response2 = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+    		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
+    		//$response2 = $bot->replyMessage($event->replyToken, $textMessageBuilder);
     		$response3 = $bot->pushMessage('R9b7dbfd03cbc9c2e4ab3624051c6b011', $textMessageBuilder);
     		$response = $bot->leaveGroup($event->source->groupId);
     	} else if("room" == $event->source->type) {
-    		//$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->source->roomId);
+    		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
     		//$response2 = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+    		$response3 = $bot->pushMessage('C56e234e2a4de4a584436e5b303f774ac', $textMessageBuilder);
     		$response = $bot->leaveRoom('R9b7dbfd03cbc9c2e4ab3624051c6b011');
     	}
     	
