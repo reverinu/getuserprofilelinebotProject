1fd1e3bf9c8d2ce9bcd571b58963bb5ef4b92bd2
reverinu <reverinudog@yahoo.co.jp>
2016-11-02 01:13:09 +0900

    cch

--- web/callback.php
+++ web/callback.php
@@ -11,9 +11,15 @@ require('../vendor/autoload.php');
 //POST
 
 $input = file_get_contents('php://input');
+
 $json = json_decode($input);
+
 $event = $json->events[0];
+
+
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('w9SmZJ6zm2ln3DRx5gw6lxNgLi5Ayjx7ftGGpyEsKhM0sGStTEdwNeu7UdSe7H3Mj7ayGjRubK0xHN7onGWxEwL6K8lHyukidy2my3LQT02u+EsRK+Mqsvj4fe0OVCIEYzFMAC+VzUTNjINaAQiRbwdB04t89/1O/w1cDnyilFU=');
+
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '3095c84a53d38913b6716fb770f3f326']);
 
 
@@ -22,35 +28,135 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '3095c84a53d38913b6716
 
 if ("message" == $event->type) {            //一般的なメッセージ(文字・イメージ・音声・位置情報・スタンプ含む)
 
-    if ("@join" == $event->message->text) {
+    if ("@bye" == $event->message->text && ("group" == $event->source->type || "room" == $event->source->type)) {
+    	/*if("group" == $event->source->type) {
+    		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
+    		$response2 = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+    		$response3 = $bot->pushMessage('R9b7dbfd03cbc9c2e4ab3624051c6b011', $textMessageBuilder);
+    		$response = $bot->leaveGroup($event->source->groupId);
+    	} else if("room" == $event->source->type) {
+    		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
+    		$response2 = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+    		$response3 = $bot->pushMessage('C56e234e2a4de4a584436e5b303f774ac', $textMessageBuilder);
+    		$response = $bot->leaveRoom('R9b7dbfd03cbc9c2e4ab3624051c6b011');
+    	}*/
+    	
+    
+    } else if ("@join" == $event->message->text) {
     	$response = $bot->getProfile($event->source->userId);
     	if ($response->isSucceeded()) {
     		$profile = $response->getJSONDecodedBody();
     		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($profile['displayName'] . "はゲームに参加したよ！");
     		$response2 = $bot->replyMessage($event->replyToken, $textMessageBuilder);
-		  }
-
+		}
+    	
     } else if ("text" == $event->message->type) {
 
-      if("group" == $event->source->type) {
-        //groupの話
-        $action = new MessageTemplateActionBuilder("NU", "nu")
-        $action1 = new MessageTemplateActionBuilder("NO", "no")
-        $action2 = new MessageTemplateActionBuilder("NE", "ne")
-
-        $button = new ButtonTemplateBuilder("ひげ", "ひげげ", "https://" . $_SERVER['SERVER_NAME'] . "/kyojin.jpeg", [$action, $action1, $action2]);
-        $button_message = new TemplateMessageBuilder("ひげがここにボタンで表示されてるよ", $button);
-
-
-        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("はなげ");
-        $response = $bot->pushMessage('R9b7dbfd03cbc9c2e4ab3624051c6b011', $textMessageBuilder);
-      } else if("room" == $event->source->type) {
-
+        //$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
+        
+        if("group" == $event->source->type) {
+    		//$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
+    		$response_format_text = [
+    			"type" => "template",
+    			"altText" => "はろはろー",
+    			"template" => [
+    				"type" => "buttons",
+    				"thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/kyojin.jpeg",
+    				"title" => "めにゅー",
+    				"text" => "めにゅーだよ！",
+    				"actions" => [
+    					[
+    						"type" => "message",
+    						"label" => "まる１",
+    						"text" => "@1"
+    					],
+    					[
+    						"type" => "message",
+    						"label" => "まる２",
+    						"text" => "@2"
+    					],
+    					[
+    						"type" => "message",
+    						"label" => "まる３",
+    						"text" => "@3"
+    					]
+    				]
+    			]
+    		];
+    		$post_data = [
+				"to" => 'R9b7dbfd03cbc9c2e4ab3624051c6b011',
+				"messages" => [$response_format_text]
+			];
+			$ch = curl_init("https://api.line.me/v2/bot/message/push");
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			    'Content-Type: application/json; charser=UTF-8',
+			    'Authorization: Bearer ' . 'w9SmZJ6zm2ln3DRx5gw6lxNgLi5Ayjx7ftGGpyEsKhM0sGStTEdwNeu7UdSe7H3Mj7ayGjRubK0xHN7onGWxEwL6K8lHyukidy2my3LQT02u+EsRK+Mqsvj4fe0OVCIEYzFMAC+VzUTNjINaAQiRbwdB04t89/1O/w1cDnyilFU='
+			    ));
+			$result = curl_exec($ch);
+			curl_close($ch);
+    		
+    		//$actionBuilders = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("ぬ！", "nu");
+    		//$buttonsMessageBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("めにゅー", "めにゅーだよ！", "https://" . $_SERVER['SERVER_NAME'] . "/kyojin.jpeg", $actionBuilders);
+    		//$response = $bot->pushMessage('R9b7dbfd03cbc9c2e4ab3624051c6b011', $textMessageBuilder);
+    	} else if("room" == $event->source->type) {
+    		//$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
+    		$response_format_text = [
+    			"type" => "template",
+    			"altText" => "はろはろー",
+    			"template" => [
+    				"type" => "buttons",
+    				"thumbnailImageUrl" => "https://" . $_SERVER['SERVER_NAME'] . "/kyojin.jpeg",
+    				"title" => "めにゅー",
+    				"text" => "めにゅーだよ！",
+    				"actions" => [
+    					[
+    						"type" => "message",
+    						"label" => "まる１",
+    						"text" => "@1"
+    					],
+    					[
+    						"type" => "message",
+    						"label" => "まる２",
+    						"text" => "@2"
+    					],
+    					[
+    						"type" => "message",
+    						"label" => "まる３",
+    						"text" => "@3"
+    					]
+    				]
+    			]
+    		];
+    		$post_data = [
+				"to" => 'C56e234e2a4de4a584436e5b303f774ac',
+				"messages" => [$response_format_text]
+			];
+			$ch = curl_init("https://api.line.me/v2/bot/message/push");
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			    'Content-Type: application/json; charser=UTF-8',
+			    'Authorization: Bearer ' . 'w9SmZJ6zm2ln3DRx5gw6lxNgLi5Ayjx7ftGGpyEsKhM0sGStTEdwNeu7UdSe7H3Mj7ayGjRubK0xHN7onGWxEwL6K8lHyukidy2my3LQT02u+EsRK+Mqsvj4fe0OVCIEYzFMAC+VzUTNjINaAQiRbwdB04t89/1O/w1cDnyilFU='
+			    ));
+			$result = curl_exec($ch);
+			curl_close($ch);
+    		
+    		
+    		//$actionBuilders = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("ぬ！", "nu");
+    		//$buttonsMessageBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("めにゅー", "めにゅーだよ！", "https://" . $_SERVER['SERVER_NAME'] . "/kyojin.jpeg", $actionBuilders);
+    		//$response = $bot->pushMessage('C56e234e2a4de4a584436e5b303f774ac', $textMessageBuilder);
     	}
 
     } else {
 
         $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ごめん、わかんなーい(*´ω｀*)");
+
     }
 
 } elseif ("follow" == $event->type) {        //お友達追加時
@@ -66,11 +172,14 @@ if ("message" == $event->type) {            //一般的なメッセージ(文字
     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Godanがいんしたお(・∀・) ');
 
 } else {
+
+    //なにもしない
+
 }
 
 
 
-$response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+//$response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
 
 syslog(LOG_EMERG, print_r($event->replyToken, true));
 
