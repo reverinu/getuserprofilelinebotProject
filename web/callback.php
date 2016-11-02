bb5ebe515b776afb232597cfaa16f879c78caf3f
reverinu <reverinudog@yahoo.co.jp>
2016-11-02 16:25:53 +0900

    DoActionAll

--- web/callback.php
+++ web/callback.php
@@ -39,78 +39,96 @@ $GAMEMODE_END = 4;//投票結果開示
 
 $gameMode = $GAMEMODE_BEFORE_THE_START;//テーブル参照してＲｏｗがあれば次行で引っ張ってくる
 
-if ($gameMode == $GAMEMODE_BEFORE_THE_START){
+if("message" == $event->type){
+  DoActionAll($event->message->text);
+  if ($gameMode == $GAMEMODE_BEFORE_THE_START){
 
-} else if ($gameMode == $GAMEMODE_WAITING) {
+  } else if ($gameMode == $GAMEMODE_WAITING) {
 
-} else if ($gameMode == $GAMEMODE_NIGHT) {
+  } else if ($gameMode == $GAMEMODE_NIGHT) {
 
-} else if ($gameMode == $GAMEMODE_NOON) {
+  } else if ($gameMode == $GAMEMODE_NOON) {
 
-} else if ($gameMode == $GAMEMODE_END){
+  } else if ($gameMode == $GAMEMODE_END){
 
+  }
 }
 
 
-//イベントタイプ判別
+// 以下関数群
 
-if ("message" == $event->type) {            //一般的なメッセージ(文字・イメージ・音声・位置情報・スタンプ含む)
-
-    if ("@join" == $event->message->text) {
-    	$response = $bot->getProfile($event->source->userId);
-    	if ($response->isSucceeded()) {
-    		$profile = $response->getJSONDecodedBody();
-    		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($profile['displayName'] . "はゲームに参加したよ！");
-    		$response2 = $bot->replyMessage($event->replyToken, $textMessageBuilder);
-		  }
-
-    } else if ("text" == $event->message->type) {
-
-      if("group" == $event->source->type) {
-
-        //groupの話
-        $action0 = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("NU", "nu");
-        $action1 = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("NO", "no");
-        $action2 = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("NE", "ne");
-
-        $button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("ひげ", "ひげげ", "https://" . $_SERVER['SERVER_NAME'] . "/kyojin.jpeg", [$action0, $action1, $action2]);
-        $button_message = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("ひげがここにボタンで表示されてるよ", $button);
-
-
-        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ぬ");
-        $response = $bot->pushMessage('R9b7dbfd03cbc9c2e4ab3624051c6b011', $button_message);
-      } else if("room" == $event->source->type) {
-
-    	}
-
-    } else {
-
-        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ごめん、わかんなーい(*´ω｀*)");
-    }
-
-} elseif ("follow" == $event->type) {        //お友達追加時
-
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("よろしくー");
-
-} elseif ("join" == $event->type) {           //グループに入ったときのイベント
-
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('こんにちは よろしくー');
-
-} elseif ('beacon' == $event->type) {         //Beaconイベント
-
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Godanがいんしたお(・∀・) ');
-
-} else {
+//全てに共通するメッセージイベント
+function DoActionAll($message){
+  if ("@help" == $message) {
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ヘルプだよ");
+    $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+  }
+  if ("@rule" == $message) {
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ルール説明だよ");
+    $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+  }
 }
 
-////////////////////////////
-//データベースとの接続を終了する場所
-////////////////////////////
-
-//$response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
-
-syslog(LOG_EMERG, print_r($event->replyToken, true));
-
-syslog(LOG_EMERG, print_r($response, true));
+//
+// //イベントタイプ判別
+//
+// if ("message" == $event->type) {            //一般的なメッセージ(文字・イメージ・音声・位置情報・スタンプ含む)
+//
+//     if ("@join" == $event->message->text) {
+//     	$response = $bot->getProfile($event->source->userId);
+//     	if ($response->isSucceeded()) {
+//     		$profile = $response->getJSONDecodedBody();
+//     		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($profile['displayName'] . "はゲームに参加したよ！");
+//     		$response2 = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+// 		  }
+//
+//     } else if ("text" == $event->message->type) {
+//
+//       if("group" == $event->source->type) {
+//
+//         //groupの話
+//         $action0 = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("NU", "nu");
+//         $action1 = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("NO", "no");
+//         $action2 = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("NE", "ne");
+//
+//         $button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("ひげ", "ひげげ", "https://" . $_SERVER['SERVER_NAME'] . "/kyojin.jpeg", [$action0, $action1, $action2]);
+//         $button_message = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("ひげがここにボタンで表示されてるよ", $button);
+//
+//
+//         $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ぬ");
+//         $response = $bot->pushMessage('R9b7dbfd03cbc9c2e4ab3624051c6b011', $button_message);
+//       } else if("room" == $event->source->type) {
+//
+//     	}
+//
+//     } else {
+//
+//         $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ごめん、わかんなーい(*´ω｀*)");
+//     }
+//
+// } elseif ("follow" == $event->type) {        //お友達追加時
+//
+//     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("よろしくー");
+//
+// } elseif ("join" == $event->type) {           //グループに入ったときのイベント
+//
+//     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('こんにちは よろしくー');
+//
+// } elseif ('beacon' == $event->type) {         //Beaconイベント
+//
+//     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Godanがいんしたお(・∀・) ');
+//
+// } else {
+// }
+//
+// ////////////////////////////
+// //データベースとの接続を終了する場所
+// ////////////////////////////
+//
+// //$response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+//
+// syslog(LOG_EMERG, print_r($event->replyToken, true));
+//
+// syslog(LOG_EMERG, print_r($response, true));
 
 return;
