d90e57a7a26444f72ff3dd0a0251b71fad8f0f91
reverinu <reverinudog@yahoo.co.jp>
2016-11-05 21:45:59 +0900

    ｇｈｄ

--- web/callback.php
+++ web/callback.php
@@ -322,10 +322,10 @@ function HandOut($num_of_people){
         //$result = mysqli_query($link, "update user set (select role from (select * from user) where game_room_num = '$game_room_num' limit '$offset_num', 1) = '$role';");
 
         //これがボタンに置き換わる
-        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($role . "なんだが");
         $result = mysqli_query($link, "select * from user where game_room_num = '$game_room_num' limit '$offset_num', 1;");
         $row = mysqli_fetch_row($result);
         $user_id = $row[1];
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($user_id . "なんだが");
         if ($i < $num_of_people) {
           $response = $bot->pushMessage($gameRoomId, $textMessageBuilder);
         }
