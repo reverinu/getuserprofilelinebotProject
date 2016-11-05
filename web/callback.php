66d2dab3969471cd78b96fd68557a9f387345aa8
reverinu <reverinudog@yahoo.co.jp>
2016-11-06 05:27:03 +0900

    ふぁふぁあ

--- web/callback.php
+++ web/callback.php
@@ -332,12 +332,16 @@ function HandOut($num_of_people){
       $result = mysqli_query($link, "select * from user where game_room_num = '$game_room_num'");
       $i = 0;
       while($row = mysqli_fetch_row($result)){
-        $role = $PEOPLE3[$i];
+        $role[$i] = $PEOPLE3[$i];
         //$role = mysqli_real_escape_string($link, $role);
-        $user_id = $row[1];
+        $user_id[$i] = $row[1];
         //$user_id = mysqli_real_escape_string($link, $user_id);
         //$result = mysqli_query($link, "update user set role = '$role' where user_id = '$user_id'");
         $i++;
+      }
+
+      for($t = 0; $t < $i; $t++){
+        $result = mysqli_query($link, "update user set role = '$role[$t]' where user_id = '$user_id[$t]'");
       }
       $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($i . "だよ！");
       $response = $bot->pushMessage("Uaa3a852ad12ceb1b4daca873a8462260", $textMessageBuilder);
